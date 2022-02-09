e50d384ecde5bd9020c532406324e2101ab38ace
prisciladuarte <duartecostap@gmail.com>
2022-02-09 19:42:36 -0300

    Sobe server.js

--- code/server.js
+++ code/server.js
@@ -1,8 +1,8 @@
-const app = require("./src/app"); //chamando o arquivo app
+const app = require("./src/app"); //chamando o arquivo app.js
 
 const PORT = 7070;
 
-//escutando a porta
+//escutando a porta 7070
 app.listen(PORT, () => {
   console.log(`Spotplay é 100% você! Estamos rodando na estação: ${PORT}`);
   
